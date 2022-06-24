ad973210dd91420826d4f297b2ee6aba7b74fba1
operantSolutions <imnotsupposedtogetjigsinit@yahoo.com>
2022-06-24 13:41:01 -0400

    Commit JS exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,11 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    let result = "";
+    for (char of str){
+        result = result.padStart((result.length + 1), char);
+    }
+    return result;
+}
 
-};
 
 // Do not edit below this line
 module.exports = reverseString;
