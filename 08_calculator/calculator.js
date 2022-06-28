5dadce4267ca5fbc0343db8d3a4f9c55d48e1998
operantSolutions <imnotsupposedtogetjigsinit@yahoo.com>
2022-06-28 17:27:03 -0400

    Complete JS exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,46 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a+b
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a-b
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  let result = 0;
+	for (const item of arr){
+    result+=item;
+  }
+  return result;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let result = 1;
+  for (const item of arr){
+    result *= item;
+  }
+  return result;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a**b;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if (num===0){
+    return 1;
+  }
+  else {
+    let result = 1;
+    let arr = [];
+    for (i=num; i>0; i--){
+      arr.push(i);
+    }
+    for (const item of arr){
+      result *=item;
+    }
+    return result;
+  }
 };
 
 // Do not edit below this line
