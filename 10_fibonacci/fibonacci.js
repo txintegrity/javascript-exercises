6c27d1edb5de84f3ff5cba9caf25a52522fefa17
operantSolutions <imnotsupposedtogetjigsinit@yahoo.com>
2022-06-29 08:38:13 -0400

    Complete JS exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,21 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    let int = parseInt(num);
+    let arr = [];
+    if (int<=0){
+        return "OOPS";
+    }
+    else{
+        for (i=int; i>0; i--){
+            if (arr.length<2){
+                arr.push(1);
+            }
+            else {
+                const nextItem = ((arr[arr.length-1]) + (arr[arr.length-2]));
+                arr.push(nextItem);
+            }
+        }
+        return arr[arr.length-1];
+    }
 };
 
 // Do not edit below this line
