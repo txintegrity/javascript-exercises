1f7c9c7e25317cc40f7df30464ad4ce2896b7946
operantSolutions <imnotsupposedtogetjigsinit@yahoo.com>
2022-06-29 09:08:06 -0400

    Complete JS exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,26 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    const today = new Date();
+    const thisYear = today.getFullYear();
+    for (const person of people){
+        if (person.yearOfDeath === undefined){
+            person.yearOfDeath = thisYear;
+        }
+    }
+    const sortedPeople = people.slice().sort((a, b) => {
+        const aAge = a.yearOfDeath - a.yearOfBirth;
+        const bAge = b.yearOfDeath - b.yearOfBirth;
+        if (aAge>bAge){
+            return 1;
+        }
+        else {
+            return -1;
+        }
+    });
+    console.table(people);
+    console.table(sortedPeople);
+    return sortedPeople[sortedPeople.length - 1];
 };
 
 // Do not edit below this line
 module.exports = findTheOldest;
+
